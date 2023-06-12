370f7f59473e23f9d5f475266ab2af32ad98be1f
Clebson Cardoso <clebsondm@gmail.com>
2023-06-12 01:06:12 -0300

    Project skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check).service(greet))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
